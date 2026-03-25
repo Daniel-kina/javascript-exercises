61d888ccdcc1c696c5e6a3e018dd932cc6459ed2
kinarkin <kinarkin@hs-pforzheim.de>
2026-03-25 16:28:17 +0100

    Exercise nr 2 done

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,26 @@
-const contains = function() {
-  
+const contains = function (obj, searchedFor) {
+  return Object.values(obj).some((element) => {
+    if (
+      element === searchedFor ||
+      (Number.isNaN(element) && Number.isNaN(searchedFor))
+    ) {
+      return true;
+    }
+
+    if (isObject(element)) {
+      return contains(element, searchedFor);
+    }
+
+    return false;
+  });
 };
-  
+
+function isObject(val) {
+  return val !== null && typeof val === "object";
+}
+
+console.log("HI");
+console.log(contains({ app: "FOO" }, "FOO"));
+
 // Do not edit below this line
 module.exports = contains;
