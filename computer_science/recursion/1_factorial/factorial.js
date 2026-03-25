477e27e4f7f0282f71f99d6ffb619c662d2e6747
kinarkin <kinarkin@hs-pforzheim.de>
2026-03-25 14:49:27 +0100

    finished exercise 1

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,17 @@
-const factorial = function() {
+const factorial = function (num) {
+  if (num < 0) {
+    return undefined;
+  }
 
+  if (!Number.isInteger(num)) {
+    return undefined;
+  }
+
+  if (num == 0) {
+    return 1;
+  } else {
+    return factorial(num - 1) * num;
+  }
 };
 
 // Do not edit below this line
